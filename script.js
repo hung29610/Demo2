4c240c00ade142253356a3435e3d0d417a00b8c8
hung29610 <honguyenhung29@gmail.com>
2025-06-23 12:48:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+  const input = document.getElementById("password").value;
+  const pass = "24062025"; // thay đổi mật khẩu tại đây
+  if (input === pass) {
+    document.getElementById("password-screen").style.display = "none";
+    document.getElementById("birthday-content").style.display = "block";
+  } else {
+    document.getElementById("message").innerText = "Sai mật khẩu rồi 😢";
+  }
+}
