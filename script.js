398e6af7a21401dd26dac566c0611b4a89edb7fd
hung29610 <honguyenhung29@gmail.com>
2025-06-23 12:52:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   const input = document.getElementById("password").value;
-  const pass = "24062025"; // thay đổi mật khẩu tại đây
+  const pass = "3007"; // thay đổi mật khẩu tại đây
   if (input === pass) {
     document.getElementById("password-screen").style.display = "none";
     document.getElementById("birthday-content").style.display = "block";
